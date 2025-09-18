0434b6d12c0a43012c9436826498b8632410b84a
h-ichi <h.ichimiya.dosapo@gmail.com>
2025-09-18 10:42:13 +0900

    APIver2

--- frontend/src/components/AkiFinder/AkiFinderPage.tsx
+++ frontend/src/components/AkiFinder/AkiFinderPage.tsx
@@ -18,7 +18,7 @@ export default function AkiFinderPage() {
 
   const fetchRanking = async () => {
     try {
-      const res = await fetch(`http://localhost:3000/scores?level=${level}`);
+      const res = await fetch(`https://aki-app-api.onrender.com/?level=${level}`);
       const data: Score[] = await res.json();
       setRanking(data); // Rails側で0以下は除外済
     } catch (err) {
